66c8e1b566843c3d10c823c03249fdef08c56eed
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-07-27 12:48:51 +0500

    navbar logo navigation

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import logo from "/github-logo.png";
+import { HomeLogo } from "./style";
 import { useNavigate } from "react-router-dom";
-import { HomeLogo, Nav } from "./style";
 
 const Logo = () => {
   const navigate = useNavigate();
 
   return (
-    <HomeLogo onClick={() => navigate("/home")}>
+    <HomeLogo onClick={() => navigate("/")}>
       <HomeLogo.Image src={logo} alt="Not Found" />
       <HomeLogo.Title>Github Finder</HomeLogo.Title>
     </HomeLogo>
